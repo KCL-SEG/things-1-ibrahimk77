f9381c212a8089444d5e5167ce303c0075fa9ceb
Ibrahim <04ibrahimkhan04@gmail.com>
2023-10-18 14:58:28 +0100

    Add restrictions to name, description,quantity

--- things/models.py
+++ things/models.py
@@ -1,10 +1,10 @@
 from django.core.validators import RegexValidator
-from django.core.validators import EmailValidator
+from django.core.validators import EmailValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 from django.db.models import Model
 from django.contrib.auth.models import AbstractUser
 
 class Thing(Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, blank=False,unique=True)
+    description = models.CharField(max_length=120, blank=True, unique=False)
+    quantity = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)])
